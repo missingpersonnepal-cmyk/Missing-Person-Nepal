446343073a93b6a52042f8589e8a867a34743fb5
kharelutshab-collab <kharelutshab@gmail.com>
2026-08-31 18:36:35 +0545

    fix: unify alembic migration heads

--- migrations/versions/e7f8a9b0c1d2_panic_alerts.py
+++ migrations/versions/e7f8a9b0c1d2_panic_alerts.py
@@ -3,7 +3,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "e7f8a9b0c1d2"
-down_revision: Union[str, None] = "f31b6c4d9201"
+down_revision: Union[str, None] = "c3d4e5f6a7b8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
